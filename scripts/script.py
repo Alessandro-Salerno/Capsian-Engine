8b54f7965d60f968b6c4c8d9dd95733d6ae64e90
John Doe <oxygen05@outlook.it>
2020-09-28 15:50:04 +0200

    even smaller changes

--- scripts/script.py
+++ scripts/script.py
@@ -5,5 +5,5 @@ camera  = PerspectiveCamera()
 window  = Window3D(camera=camera, vsync=False)
 
 def start():
-    Log.critical("Test error")
+    Log.critical("This is just a test for the new Death Screen, you can check the script to remove it and the options.cpsn file to change script all together")
 engine.entries + start

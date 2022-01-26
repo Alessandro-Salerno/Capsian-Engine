2393330dfa1a278a00249f4a0e357141d8afa57f
lorenzo <lorenzo.bergadano@intesa.it>
2022-01-26 22:39:36 +0100

    fix: use different type of pip based on os name

--- prepare.py
+++ prepare.py
@@ -1,12 +1,15 @@
-from os import system
-
+import os
 
 print("Starting Capsian Preparation Tool...")
 print("This script will install all the dependencies you need")
 input("Press enter to continue or lcose to terminate ")
 
-system("pip install pyglet==1.5.6")
-system("pip install PyOpenGL")
-system("pip install pyinstaller")
+_pip_type = "pip"
+if os.name == "posix":
+    _pip_type = "pip3"
+
+os.system(_pip_type + " install pyglet==1.5.6")
+os.system(_pip_type + " install PyOpenGL")
+os.system(_pip_type + " install pyinstaller")
 
 input("Installation completed!\nPress enter to exit ")

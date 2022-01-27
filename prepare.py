1ce3443c9be6e946f59dfef7a88476bc2ce88c5e
tzyvoski <OxyGen05@outlook.it>
2022-01-27 13:16:18 +0100

    Typos fixed

--- prepare.py
+++ prepare.py
@@ -1,8 +1,8 @@
 import os
 
-print("Starting Capsian Preparation Tool...")
+print("Starting Capsian Setup Tool...")
 print("This script will install all the dependencies you need")
-input("Press enter to continue or lcose to terminate ")
+input("Press enter to continue or close to terminate ")
 
 _pip_type = "pip"
 if os.name == "posix":
@@ -12,4 +12,4 @@ os.system(_pip_type + " install pyglet==1.5.6")
 os.system(_pip_type + " install PyOpenGL")
 os.system(_pip_type + " install pyinstaller")
 
-input("Installation completed!\nPress enter to exit ")
+input("Installation complete!\nPress enter to exit ")
